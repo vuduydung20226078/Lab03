567ff69c494c6e446e8d5fb6a597218922a50b44
vuduydung20226078 <vuduydung08111997@gmail.com>
2024-11-26 04:21:37 +0700

    Add part class-member

--- src/DigitalVideoDisc.java
+++ src/DigitalVideoDisc.java
@@ -5,8 +5,13 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
-	
-    
+	private int id; // Instance member: Unique ID for each DVD
+	  // Class member
+    private static int nbDigitalVideoDiscs = 0; // Class member: Tracks number of DVDs created
+
+    public int getId() {
+        return id; // Getter for instance member ID
+    }
 	public String getTitle() {
 		return title;
 	}
@@ -38,28 +43,43 @@ public class DigitalVideoDisc {
 		this.cost = cost;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.length = length;
-		this.cost = cost;
-	}
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+
+        // Increment class member and assign ID
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
 	public DigitalVideoDisc(String title, String category, int length, float cost) {
 		this.title = title;
 		this.category = category;
 		this.length = length;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+	    this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+	    this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title) {
 		this.title = title;
+		nbDigitalVideoDiscs++;
+	    this.id = nbDigitalVideoDiscs;
 		
 	}
+	public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
 	public void displayInfo() {
+		System.out.println("ID: " + id); // Print unique ID
         System.out.println("Title: " + title);
         System.out.println("Category: " + category);
         System.out.println("Director: " + director);
